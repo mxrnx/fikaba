00e69aae244add95125a48ca443073544f2fccaa
knarka <knarka@users.noreply.github.com>
2016-10-27 22:49:25 +0200

    add user-definable footer text

--- config.example.php
+++ config.example.php
@@ -6,6 +6,7 @@ define(SHOWTITLETXT, 1);			//Show TITLE at top (1: yes  0: no)
 define(SHOWTITLEIMG, 0);			//Show image at top (0: no, 1: single, 2: rotating)
 define(TITLEIMG, 'title.jpg');			//Title image (point to php file if rotating)
 define(LANGUAGE, 'en');				//Language. Fikaba provides en and jp by default
+define(FOOTTEXT, '');				//Extra footer text, for disclaimers and such. Can be left empty
 
 // Database settings
 define(POSTTABLE, 'CHANGEME');			//Post table (NOT DATABASE) used by image board
