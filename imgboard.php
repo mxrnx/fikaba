a80fc0fa2a517a7a85ecad988e25455d28f4bccc
knarka <knarka@users.noreply.github.com>
2016-11-07 19:35:23 +0100

    fix thumbnails, fit oekaki into page better

--- imgboard.php
+++ imgboard.php
@@ -376,7 +376,7 @@ function form(&$dat,$resno,$admin=""){
 	<tr><td class="postblock">'.S_SUBJECT.'</td><td><input type="text" name="sub" size="35" />
 	<input type="submit" value="'.S_SUBMIT.'" /></td></tr>
 	<tr><td class="postblock">'.S_COMMENT.'</td><td><textarea id="com" name="com" cols="50" rows="4"></textarea></td></tr>';
-	if(OEKAKI_ENABLED){$dat.='<tr><td class="postblock">'.S_OEKAKI.'</td><td id="oekakiparent"><script type="text/javascript">function startoekaki(){Ritare.start({parentel:"oekakiparent",onFinish:function(e){newfield=document.createElement("input");newfield.type="hidden";newfield.name="oekaki";newfield.id="oekakifile";newfield.value=(Ritare.canvas.toDataURL(\'image/png\')); document.getElementById("postform").appendChild(newfield);alert(\'Oekaki saved!\');},width:600,height:300});};</script><p id="oekakistarter" onclick="startoekaki();var el = document.getElementById(\'oekakistarter\');el.parentNode.removeChild(el);var el = document.getElementById(\'filerow\');el.parentNode.removeChild(el)">'.S_OEKAKILOAD.'</p></td></tr>';}
+	if(OEKAKI_ENABLED){$dat.='<tr><td class="postblock">'.S_OEKAKI.'</td><td id="oekakiparent"><script type="text/javascript">function startoekaki(){Ritare.start({parentel:"oekakiparent",onFinish:function(e){newfield=document.createElement("input");newfield.type="hidden";newfield.name="oekaki";newfield.id="oekakifile";newfield.value=(Ritare.canvas.toDataURL(\'image/png\')); document.getElementById("postform").appendChild(newfield);alert(\'Oekaki saved!\');},width:370,height:300});};</script><p id="oekakistarter" onclick="startoekaki();var el = document.getElementById(\'oekakistarter\');el.parentNode.removeChild(el);var el = document.getElementById(\'filerow\');el.parentNode.removeChild(el)">'.S_OEKAKILOAD.'</p></td></tr>';}
 	$dat.='<tr id="filerow"><td class="postblock">'.S_UPLOADFILE.'</td>
 <td><input type="file" name="upfile" size="35" />';
 	if(!$resno && !FORCEIMAGE){$dat.='[<label><input type="checkbox" name="textonly" value="on" />'.S_NOFILE.'</label>]';}
@@ -763,7 +763,7 @@ function thumb($path,$tim,$ext){
 	// width, height, and type are aquired
 	$size = GetImageSize($fname);
 	switch ($size[2]) {
-	case IMG_GIF:
+	case 1:
 		if(function_exists("ImageCreateFromGIF")){
 			$im_in = @ImageCreateFromGIF($fname);
 			if($im_in){break;}
@@ -773,9 +773,9 @@ function thumb($path,$tim,$ext){
 		unlink($path.$tim.'.png');
 		if(!$im_in)return;
 		break;
-	case IMG_JPG: $im_in = @ImageCreateFromJPEG($fname);
+	case 2: $im_in = @ImageCreateFromJPEG($fname);
 		if(!$im_in){return;} break;
-	case IMG_PNG:
+	case 3:
 		if(!function_exists("ImageCreateFromPNG"))return;
 		$im_in = @ImageCreateFromPNG($fname);
 		if(!$im_in){return;}
