4c3f3a1febed9764bbcfb2eb1d465eb8060ba5fa
knarka <knarka@users.noreply.github.com>
2017-06-28 22:25:15 +0200

    use minified jscolor

--- imgboard.php
+++ imgboard.php
@@ -355,7 +355,7 @@ function head(&$dat){
 <script type="text/javascript"><!--
 function l(e){var P=getCookie("pwdc"),N=getCookie("namec"),i;with(document){for(i=0;i<forms.length;i++){if(forms[i].pwd)with(forms[i]){if(!pwd.value)pwd.value=P;}if(forms[i].name)with(forms[i]){if(!name.value)name.value=N;}}}};function getCookie(key, tmp1, tmp2, xx1, xx2, xx3) {tmp1 = " " + document.cookie + ";";xx1 = xx2 = 0;len = tmp1.length;	while (xx1 < len) {xx2 = tmp1.indexOf(";", xx1);tmp2 = tmp1.substring(xx1 + 1, xx2);xx3 = tmp2.indexOf("=");if (tmp2.substring(0, xx3) == key) {return(unescape(tmp2.substring(xx3 + 1, xx2 - xx1 - 1)));}xx1 = xx2 + 1;}return("");}
 //--></script><script type="text/javascript">function addref(postid) {document.getElementById("com").value += ">>" + postid + "\n";}</script>';
-	if(OEKAKI_ENABLED){$dat.='<script type="text/javascript" src="js/ritare/jscolor/jscolor.js"></script><script type="text/javascript" src="js/ritare/ritare.js"></script><link rel="stylesheet" type="text/css" href="js/ritare/ritare.css" />';}
+	if(OEKAKI_ENABLED){$dat.='<script type="text/javascript" src="js/ritare/jscolor/jscolor.min.js"></script><script type="text/javascript" src="js/ritare/ritare.js"></script><link rel="stylesheet" type="text/css" href="js/ritare/ritare.css" />';}
 	$dat.='</head>
 	<body>
 	<div class="styles"><select>
